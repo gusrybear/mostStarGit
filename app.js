9c4b60c8b02511ad27dcf77fa150dc7a5e01e19e
Chalermkiat Tiawpanichkij <extreme.gamie@gmail.com>
2024-04-04 00:06:05 +0700

    create cli app about get most stargazer repositories by two dates(startDate, endDate)

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
-const express = require("express")
+const readline = require("node:readline")
 
-const app = express()
-const PORT = 8080
+const rl = readline.createInterface({
+  input: process.stdin,
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+  output: process.stdout,
 })
 
-app.get("/api", (req, res) => {
-  res.send("user on path /api")
-})
+const baseUrl = `https://api.github.com/search/repositories`
 
-app.listen(PORT, () => {
-  console.log(`Server is running on Port:${PORT}`)
-})
+const getStrDate = (text) => {
+  return text.split("-").join("")
+}
 
-module.exports = app
+const isLessThanOrEqual = (value1, value2) => {
+  return parseInt(value1) <= parseInt(value2)
+}
+
+rl.question(`What is Start Date(YYYY-MM-DD)?`, (startDate) => {
+  rl.question(`What is End Date(YYYY-MM-DD)?`, (endDate) => {
+    if (isLessThanOrEqual(getStrDate(startDate), getStrDate(endDate))) {
+      const query = `?q=created:${startDate}..${endDate}&sort=stars&order=desc`
+      const url = baseUrl + query
+      const headers = {
+        "X-GitHub-Api-Version": "2022-11-28",
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      }
+
+      fetch(url, { method: "GET", headers: headers })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`Network response not ok.`)
+          }
+
+          return res.json()
+        })
+
+        .then((data) => {
+          for (const repo of data.items) {
+            console.log(
+              `(${repo.stargazers_count}) ${repo.name} => ${repo.description}`
+            )
+          }
+        })
+
+        .catch((error) => {
+          console.log(`Error:`, error)
+        })
+    } else {
+      console.log(`Error start date is chosen after end date`)
+    }
+
+    rl.close()
+  })
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+Before start app You need to create .env file with GITHUB_TOKEN like
+
+## GITHUB_TOKEN=abc...xyz
+
+Start app with this command
+
+## node --env-file=.env app.js
